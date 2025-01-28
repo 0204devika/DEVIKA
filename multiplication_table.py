296fa5c7243690a075f22764ad95db05927b1d9c
0204devika <144982084+0204devika@users.noreply.github.com>
2025-01-28 13:38:51 +0530

    commit

--- multiplication_table.py
+++
@@ -1,6 +0,0 @@
-def main():
-    n=int(input("enter the number:"))      #input of number for which table will be printed
-    rng=int(input("enter the range:"))     #input of range upto which table will be printed
-    for i in range(1,rng+1):
-        print(f"{n}x{i}={n*i}")
-main()
